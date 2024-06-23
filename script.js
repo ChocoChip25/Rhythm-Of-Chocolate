28728a57292027795a95edc9c73af56e15375096
ChocoChip25 <treshanasus@gmail.com>
2024-06-23 20:21:01 +0530

    site

---
+++ script.js
@@ -0,0 +1,12 @@
+function toggleLanguageDropdown() {
+  var dropdown = document.getElementById("languageDropdown");
+  dropdown.classList.toggle("show");
+}
+
+function changeLanguage(lang) {
+  // Implement logic to change language
+  // For demonstration purposes, you can alert the selected language
+  alert("Language changed to: " + lang);
+}
+
+
